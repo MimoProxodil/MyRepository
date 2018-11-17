dcb30b06bcac7b903279551702d7b42763abcc22
MimoProxodil <andreykotor@gmail.com>
2018-11-17 17:14:32 +0400

    Update User.java

--- User.java
+++ User.java
@@ -1,9 +1,15 @@
 public class User
 {
-int age;//поле
-String firstName;//поле
-String lastName;//поле
-User()
-{// конструктор по умолчанию
-}
+  int age;//поле
+  String firstName;//поле
+  String lastName;//поле
+  User()// конструктор по умолчанию
+  {
+  }
+  public User(int age, String firstName, String lastName) {//конструктор с тремя параметрами
+  { 
+    this.age = age;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
