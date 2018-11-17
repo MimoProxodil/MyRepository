c2b31872632495723f9fac46c799872c9af35dd7
MimoProxodil <andreykotor@gmail.com>
2018-11-17 20:58:32 +0400

    Update User.java
    Добавлен метод вывода.

--- User.java
+++ User.java
@@ -1,9 +1,12 @@
 public class User
 {
-int age;//поле
-String firstName;//поле
-String lastName;//поле
-User()
-{// конструктор по умолчанию
-}
+  int age;//поле
+  String firstName;//поле
+  String lastName;//поле
+  User()
+  {// конструктор по умолчанию
+  }
+ String say() {//метод
+  return "Мне "+age+" лет, я "+firstName+" "+lastName;
+ }
 }
